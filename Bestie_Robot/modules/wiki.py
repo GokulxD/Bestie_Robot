a7d6dd2d6812a249c86c73045e801b9aab8e6e1f
Aasf-Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2022-06-14 13:40:11 +0530

    Update wiki.py

--- Bestie_Robot/modules/wiki.py
+++ Bestie_Robot/modules/wiki.py
@@ -1,55 +1,45 @@
 import wikipedia
-from Bestie_Robot import dispatcher
-from Bestie_Robot.modules.disable import DisableAbleCommandHandler
-from telegram import ParseMode, Update
-from telegram.ext import CallbackContext, run_async
-from wikipedia.exceptions import DisambiguationError, PageError
+import re
+
+from Bestie_Robot.modules.helper_funcs.decorators import bestie_cmd
+from telegram.error import BadRequest
+from telegram.ext import CallbackContext
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+
+from telegram.constants import ParseMode
 
 
-def wiki(update: Update, context: CallbackContext):
-    msg = (
-        update.effective_message.reply_to_message
-        if update.effective_message.reply_to_message
-        else update.effective_message
-    )
-    res = ""
-    if msg == update.effective_message:
-        search = msg.text.split(" ", maxsplit=1)[1]
+@bestie_cmd(command='wiki', can_disable=True)
+async def wiki(update: Update, context: CallbackContext):
+    kueri = re.split(pattern="wiki", string = update.effective_message.text)
+    message = update.effective_message
+    wikipedia.set_lang("en")
+    if not str(kueri[1]):
+        await update.effective_message.reply_text("Enter keywords!")
     else:
-        search = msg.text
-    try:
-        res = wikipedia.summary(search)
-    except DisambiguationError as e:
-        update.message.reply_text(
-            "Disambiguated pages found! Adjust your query accordingly.\n<i>{}</i>".format(
-                e
-            ),
-            parse_mode=ParseMode.HTML,
-        )
-    except PageError as e:
-        update.message.reply_text(
-            "<code>{}</code>".format(e), parse_mode=ParseMode.HTML
-        )
-    if res:
-        result = f"<b>{search}</b>\n\n"
-        result += f"<i>{res}</i>\n"
-        result += f"""<a href="https://en.wikipedia.org/wiki/{search.replace(" ", "%20")}">Read more...</a>"""
-        if len(result) > 4000:
-            with open("result.txt", "w") as f:
-                f.write(f"{result}\n\nUwU OwO OmO UmU")
-            with open("result.txt", "rb") as f:
-                context.bot.send_document(
-                    document=f,
-                    filename=f.name,
-                    reply_to_message_id=update.message.message_id,
-                    chat_id=update.effective_chat.id,
-                    parse_mode=ParseMode.HTML,
-                )
-        else:
-            update.message.reply_text(
-                result, parse_mode=ParseMode.HTML, disable_web_page_preview=True
+        try:
+            pertama = await update.effective_message.reply_text("🔄 Loading...")
+            keyboard = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="🔧 More Info...", url=wikipedia.page(kueri).url
+                        )
+                    ]
+                ]
             )
-
-
-WIKI_HANDLER = DisableAbleCommandHandler("wiki", wiki, run_async=True)
-dispatcher.add_handler(WIKI_HANDLER)
+            context.bot.editMessageText(
+                chat_id=update.effective_chat.id,
+                message_id=pertama.message_id,
+                text=wikipedia.summary(kueri, sentences=10),
+                reply_markup=keyboard,
+            )
+        except wikipedia.PageError as e:
+            await message.reply_text(f"⚠ Error: {e}")
+        except BadRequest as et:
+            await message.reply_text(f"⚠ Error: {et}")
+        except wikipedia.exceptions.DisambiguationError as eet:
+            await message.reply_text(
+                f"⚠ Error\n There are too many query! Express it more!\nPossible query result:\n{eet}"
+            )
+            """
