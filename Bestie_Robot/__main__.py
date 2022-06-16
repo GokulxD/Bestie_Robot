3f570d5c8e112be797899d9cbbf48fc5b5ce1e75
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-06-16 19:32:54 +0530

    Update __main__.py

--- Bestie_Robot/__main__.py
+++ Bestie_Robot/__main__.py
@@ -122,10 +122,10 @@ buttons = [
                     [                  
                        InlineKeyboardButton(                         
                              text="🚑 Support",
-                             url="https://t.me/bestie_support",
+                             url="https://t.me/bestie_support"),
                        InlineKeyboardButton(
                              text="📢 Updates",
-                             url="https://t.me/Bestie_Updates"
+                             url="https://t.me/Bestie_Updates")
                      ], 
     ]
 
