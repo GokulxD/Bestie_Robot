57ef49eed3953e22c8a190919099bdee287c8a53
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-06-17 11:45:29 +0530

    Update time.py

--- Bestie_Robot/modules/time.py
+++ Bestie_Robot/modules/time.py
@@ -2,8 +2,8 @@ from Bestie_Robot import pbot
 from pyrogram import filters
 from pyrogram.types import Message
 
-@pbot.on_message(filters.command("whois", prefixes=["/", ".", "?", "-"]))
-async def whois(_, m: Message):
+@pbot.on_message(filters.command("when", prefixes=["/", ".", "?", "-"]))
+async def when(_, m: Message):
     if m.reply_to_message:
         await pbot.send_message(m.reply_to_message.date)
         return
