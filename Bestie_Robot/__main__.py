94b645f1933f748757ea34f6b133bf5bee0c9804
ロックマディ | Maddie <98200081+MaddieTheRock@users.noreply.github.com>
2022-08-21 14:34:06 +0530

    Update __main__.py

--- Bestie_Robot/__main__.py
+++ Bestie_Robot/__main__.py
@@ -95,7 +95,7 @@ START_MSG = "I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>"
 PM_START_TEXT = """
 ────「 [{}](https://telegra.ph/file/491514bc7de907899a4b0.jpg)」────
 *Hola! {},*
-*I am an Anime themed advance group management bot with a lot of  Features.*
+*I am an Anime themed advanced group management bot with a lot of  Features.*
 ➖➖➖➖➖➖➖➖➖➖➖➖➖
 • *Uptime:* `{}`
 • `{}` *users, across* `{}` *chats.*
